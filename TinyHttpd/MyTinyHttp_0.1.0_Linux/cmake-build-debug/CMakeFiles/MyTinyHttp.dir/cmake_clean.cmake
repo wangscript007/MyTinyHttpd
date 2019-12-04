5b8592ae6f4424b4a1a8823a972db8aac7896156
Ran Miller <z861983448@gmai.com>
2019-12-04 19:35:32 +0800

    Modify configuration

---
+++ TinyHttpd/MyTinyHttp_0.1.0_Linux/cmake-build-debug/CMakeFiles/MyTinyHttp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyTinyHttp.dir/Src/main.cpp.obj"
+  "MyTinyHttp.exe"
+  "MyTinyHttp.exe.manifest"
+  "MyTinyHttp.pdb"
+  "libMyTinyHttp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyTinyHttp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ TinyHttpd/MyTinyHttp_0.1.0_Linux/cmake-build-debug/CMakeFiles/MyTinyHttp.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for MyTinyHttp.
+# This may be replaced when dependencies are built.
